e2e2f2350c51d78cea37c6a00dc9872b260fcd86
Jiahao Chen2 <shubing.yangca@gmail.com>
2022-07-06 15:03:20 -0300

    Add publisher and subscriber into navigation goal file

--- src/simple_navigation_goals.cpp
+++ src/simple_navigation_goals.cpp
@@ -1,15 +1,28 @@
 #include <ros/ros.h>
+#include <std_msgs/string.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+
+
 //define actionlib from move_base_msgs library
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>
 MoveBaseClient;
+
+
+// Global Functions for subscriber
+void chatterCallback(const std_msgs::String::ConstPtr& msg)
+{
+   ROS_INFO("I heard: [%s]", msg->data.c_str());
+}
+
+
 //Main function to control the robot move at four waypoints
 //Update the map information
 int main(int argc, char** argv){
   ros::init(argc, argv, "simple_navigation_goals");
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
+
   //wait for the action server to come up
   while(!ac.waitForServer(ros::Duration(5.0))){
 	ROS_INFO("Waiting for the move_base action server to come up");
@@ -20,6 +33,12 @@ int main(int argc, char** argv){
   double wayppoints[8][3]={{1.5211,0.4,1.5708},{1.5211,2.0,3.1415},{0.2721,2.0,4.7124},{0.2721,1.2322,6.2832}};
   //we'll send a goal to the robot to move 1 meter forward
   for(int i = 0 ; i<5 ;i++){
+
+
+	    ros::NodeHandle n;
+  	    ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+
+
 	    //Read map information
             goal_point[i].target_pose.header.frame_id = "map";
  	    //Setup the sychronus time
